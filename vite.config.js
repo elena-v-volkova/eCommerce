d1459beec63b242d663173a901cb6cadb4a1edff
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-05-18 22:43:50 +0200

    feat: add new zod validation

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tsconfigPaths from 'vite-tsconfig-paths';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    base: '/',
+    server: {
+        open: '/',
+        hmr: true,
+    },
+    root: './src/',
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
+    },
+    envDir: '../',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './vitest.setup.ts',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'html'],
+        },
+        exclude: [],
+    },
+});
