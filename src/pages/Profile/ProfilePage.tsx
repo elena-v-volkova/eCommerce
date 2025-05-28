46c8df883d95cd1fad555710a43c7152677b6f1c
artsemrogovenko <taftoos2@gmail.com>
2025-05-28 18:57:42 +0300

    feat: add menu to user page

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,5 +1,60 @@
+import { useEffect, useState } from 'react';
+import { Customer } from '@commercetools/platform-sdk';
+import { UserRoundCog } from 'lucide-react';
+
+import { Selectors } from './Selectors';
 import styles from './ProfilePage.module.scss';
 
+import { useAuth } from '@/shared/model/AuthContext';
 export function Profile() {
-  return <div className={styles.profile}>profile page</div>;
+  const { user } = useAuth();
+  const [subpage, setSubpage] = useState<string>('');
+  const [content, setContent] = useState<React.ReactNode>(null);
+
+  useEffect(() => {
+    switch (subpage) {
+      case 'addresses':
+        setContent(AddressesContent(user));
+        break;
+      case 'password':
+        setContent(PasswordContent);
+        break;
+      case 'logout':
+        break;
+      case 'personal':
+        setContent(PersonalInfoContent(user));
+        break;
+      default:
+        setContent(
+          <UserRoundCog className="size-full max-w-[350px] stroke-[#eeeec3] dark:stroke-[#777773]" />,
+        );
+        break;
+    }
+  }, [subpage]);
+
+  return (
+    <>
+      <h2 className="mb-10 flex w-full justify-center  self-center text-[2.3rem]   font-semibold leading-9 lg:text-5xl  ">
+        User Profile Page
+      </h2>
+      <div className={styles.profile}>
+        <Selectors onAction={(key) => setSubpage(key.toString())} />
+        <div className="mx-[20px] flex    min-h-[180px] w-full justify-center  ">
+          {content}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function PersonalInfoContent(user: Customer | null) {
+  return <div>Your Information</div>;
+}
+
+function AddressesContent(user: Customer | null) {
+  return <div>Manage addresses</div>;
+}
+
+function PasswordContent() {
+  return <div>Write new password</div>;
 }
