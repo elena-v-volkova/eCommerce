4865e724e2e96951e042f2a3e106d18061826c5f
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-06-03 14:53:44 +0200

    feat: add new Profile Page and fix linter product page

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,5 +1,11 @@
 import styles from './ProfilePage.module.scss';
 
+import UserProfilePage from '@/components/ProfilePage/UserProfilePage';
+
 export function Profile() {
-  return <div className={styles.profile}>profile page</div>;
+  return (
+    <div className={styles.profile}>
+      <UserProfilePage />
+    </div>
+  );
 }
