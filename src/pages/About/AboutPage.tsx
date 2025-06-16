c249d6889724ad80660e3220730b16ae97f8b917
Vl Lena <tg @vlpro>
2025-06-17 01:10:03 +0300

    fix: change photo routs

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Card, Avatar, Link } from '@heroui/react';
+import lena from '../../assets/lena.jpg';
+import artsem from '../../assets/artsem.jpeg';
+import andrej from '../../assets/andrej.jpg';
 
 interface TeamMember {
   name: string;
@@ -14,21 +17,21 @@ const team: TeamMember[] = [
     name: 'Andrej Dortmann',
     role: 'Frontend Engineer & team lead',
     bio: 'Always ready to lend a hand and explain complex concepts. An excellent team lead and technical specialist.',
-    photoSrc: '../../assets/andrej.jpg',
+    photoSrc: andrej,
     githubUrl: 'https://github.com/FranticMario',
   },
   {
     name: 'Elena Volkova',
     role: 'Frontend Engineer',
     bio: 'Great technical specialist delivering fast, high-quality, and cost-effective solutions.',
-    photoSrc: '../../assets/lena.jpg',
+    photoSrc: lena,
     githubUrl: 'https://github.com/elena-v-volkova',
   },
   {
     name: 'Artsem Rogovenko',
     role: 'Frontend Engineer',
     bio: 'Highly responsible and deeply invested in the project’s success, with strong problem-solving and communication skills.',
-    photoSrc: '../../assets/artsem.jpeg',
+    photoSrc: artsem,
     githubUrl: 'https://github.com/artsemrogovenko',
   },
 ];
